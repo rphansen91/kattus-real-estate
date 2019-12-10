e1d993303946274252be14f306c29a3e0c3fa210
Ryan Hansen <ryanhansen@rhansen-mac.attlocal.net>
2019-12-10 01:52:16 -0800

    Css fixes

--- src/components/PreviewCompatibleImage.js
+++ src/components/PreviewCompatibleImage.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import Img from 'gatsby-image'
 
 const PreviewCompatibleImage = ({ imageInfo }) => {
-  const imageStyle = { borderRadius: '5px 0' }
+  const imageStyle = { borderRadius: '5px 5px 0 0' }
   const { alt = '', childImageSharp, image } = imageInfo
 
   if (!!image && !!image.childImageSharp) {
